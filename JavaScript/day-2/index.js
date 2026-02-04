86d12485004bace51727e5639f96fe9b6710db26
Sruthi Sri <gsruthisri49@gmail.com>
2026-02-04 19:20:44 +0530

    js day 2 task added

---
+++ JavaScript/day-2/index.js
@@ -0,0 +1,136 @@
+//1
+// var x;
+// x=5;
+// var x=10;
+// console.log(x);//10
+
+
+
+// console.log(x);//undefined
+// var x=15;
+
+
+// var b;
+// console.log(b);//undefined
+
+// var a=1;
+// {
+//     var a=2;
+// }
+// console.log(a);//2
+
+// {
+//     var c=10;
+// }
+// console.log(c);//10
+
+// var p;
+// console.log(p);//undefined
+// p=20;
+
+
+// var q=5;
+// var q=10;
+// console.log(q);//10
+
+// var r;
+//  r = 30;
+//  var r = 50;
+
+//  console.log(r);//50
+
+//  	console.log(x);
+//  var x = 25;//undefined
+
+
+// console.log(a);
+//  var a = 40;
+
+// var t = 10; 
+//  var t; //skipped
+//  console.log(t);//10
+
+// x = 100; 
+//  var x; 
+//  console.log(x);//100
+
+
+
+// var y = 5; 
+//  var y = 10; 
+//  console.log(y);//10
+
+
+
+// var z = 1; 
+//  z = 2; 
+//  console.log(z);//2
+
+
+// var _Xyz12$;
+// _Xyz12$ = 45;
+//  console.log(_Xyz12$);//45
+
+
+
+// var $num_Val = 8; 
+//  $num_Val = 16; 
+//  console.log($num_Val);//16
+
+
+// console.log(_x_Val12); 
+//  var _x_Val12 = 27; //undefined
+
+
+
+// var Val_12X$ = 10; 
+//  Val_12X$ = 20; 
+//  console.log(Val_12X$);//20
+
+
+// console.log(VAL_num); //undefined
+//  var VAL_num = "Test"; 
+
+
+// var $count = 5; 
+//  $count = $count + 3; 
+//  console.log($count);//8
+
+
+// var _alpha$ = 50; 
+//  var _alpha$ = 100; 
+//  console.log(_alpha$);//100
+
+
+// var A1b_2$ = 1; 
+//  console.log(A1b_2$);//1
+
+
+
+// var $$$ = 99; 
+//  $$$ = 88; 
+//  console.log($$$);//88
+
+// var _vA_rX = "Code"; 
+//  console.log(_vA_rX);//code
+
+
+// var X_$12; 
+//  console.log(X_$12); //undefined
+//  X_$12 = 123;
+
+
+// var __X_var$ = "Hello"; 
+// var __X_var$ = "World"; 
+// console.log(__X_var$);//world
+
+
+// var VaR_$$ = "Testing"; 
+//  console.log(VaR_$$);//testing
+
+// console.log($$$Alpha); //undefined
+//  var $$$Alpha = 55; 
+
+
+// var 12x$var_ = 100; 
+//  console.log(12x$var_);//error
